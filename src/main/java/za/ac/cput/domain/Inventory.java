85983aaa3a353a1400977822c0c350d22a81e517
siyathandaza <smqhamane98@gmail.com>
2023-03-24 20:35:42 +0200

    Inventory class and sales class added

--- src/main/java/za/ac/cput/domain/Inventory.java
+++ src/main/java/za/ac/cput/domain/Inventory.java
@@ -1,55 +1,76 @@
 package za.ac.cput.domain;
 
 public class Inventory {
-    private String itemName;
+
+    private String productId;
     private int quantity;
-    private double price;
+    private int reorderQuantity;
 
-    private Inventory(String itemName, int quantity, double price ) {
-        this.itemName = itemName;
+    private Inventory(String productId, int quantity, int reorderQuantity) {
+        this.productId = productId;
         this.quantity = quantity;
-        this.price = price;
-
+        this.reorderQuantity = reorderQuantity;
+    }
+    private  Inventory(Builder builder) {
+        this.productId = builder.productId;
+        this.quantity = builder.quantity;
+        this.reorderQuantity = builder.reorderQuantity;
     }
 
-    public String getItemName() {
-        return itemName;
+    public String getProductId() {
+        return productId;
     }
 
     public int getQuantity() {
         return quantity;
     }
 
-    public double getPrice() {
-        return price;
+    public int getReorderQuantity() {
+        return reorderQuantity;
     }
 
+    @Override
+    public String toString() {
+        return "Inventory{" +
+                "productId='" + productId + '\'' +
+                ", quantity=" + quantity +
+                ", reorderQuantity=" + reorderQuantity +
+                '}';
+    }
 
-    public static class InventoryBuilder {
-        private String itemName;
+    public static class Builder {
+        private String productId;
         private int quantity;
-        private double price;
+        private int reorderQuantity;
 
-        public InventoryBuilder setItemName(String itemName) {
-            this.itemName = itemName;
+        public Builder setProductId(String productId) {
+            this.productId = productId;
             return this;
         }
 
-        public InventoryBuilder setQuantity(int quantity) {
+        public Builder setQuantity(int quantity) {
             this.quantity = quantity;
             return this;
         }
 
-        public InventoryBuilder setPrice(double price) {
-            this.price = price;
+        public Builder setReorderQuantity(int reorderQuantity) {
+            this.reorderQuantity = reorderQuantity;
+            return this;
+        }
+        public Builder copy (Inventory inventory){
+            this.productId = productId;
+            this.quantity = quantity;
+            this.reorderQuantity = reorderQuantity;
             return this;
         }
 
 
         public Inventory build() {
-            return new Inventory(itemName, quantity, price);
+            return new Inventory(productId, quantity, reorderQuantity);
         }
-
-
     }
 }
+
+
+
+
