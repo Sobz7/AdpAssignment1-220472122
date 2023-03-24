4881af12bb5559163806a8a181f52233f7e90cd9
Sobz7 <101484085+Sobz7@users.noreply.github.com>
2023-03-25 00:23:40 +0200

    Update Payment.java

--- src/main/java/za/ac/cput/domain/Payment.java
+++ src/main/java/za/ac/cput/domain/Payment.java
@@ -61,4 +61,51 @@ public class Payment {
                 ", paymentMethod='" + paymentMethod + '\'' +
                 '}';
     }
+     public static class Builder {
+
+        private String paymentID;
+        private String orderID;
+        private LocalDate paymentDate;
+        private Integer paymentAmount;
+         private String paymentMethod;
+
+
+
+        public Builder setpaymentID(String paymentID) {
+            this.paymentID = paymentID;
+            return this;
+        }
+        public Builder setorderID(String orderID) {
+            this.orderID = orderID;
+            return this;
+        }
+
+        public Builder setpaymentDate(LocalDate paymentDate) {
+            this.paymentDate = paymentDate;
+            return this;
+        }
+        public Builder setpaymentAmount(Integer paymentAmount) {
+            this.paymentAmount = paymentAmount;
+            return this;
+        }
+        public Builder setpaymentMethod(String paymentMethod) {
+            this.paymentMethod = paymentMethod;
+            return this;
+        }
+        public Builder copy (Payment payment){
+            this.paymentID = payment.paymentID;
+            this.orderID = payment.orderID;
+            this.paymentAmount = payment.paymentAmount;
+            this.paymentDate = payment.paymentDate;
+            this.paymentMethod = payment.paymentMethod;
+            return this;
+        }
+
+        public Payment build(){
+            return new Payment(this);
+        }
+    }
+
+
+
 }
