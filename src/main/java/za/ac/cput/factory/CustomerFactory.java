925db9111c0e9d12a4690df2e1e6bc1990ec6b6c
Jordy Meye <220072841@mycput.ac.za>
2023-03-25 12:20:33 +0200

    factory package added and customer factory class added

---
+++ src/main/java/za/ac/cput/factory/CustomerFactory.java
@@ -0,0 +1,17 @@
+package za.ac.cput.factory;
+
+import za.ac.cput.domain.Customer;
+import za.ac.cput.util.Helper;
+
+public class CustomerFactory {
+
+    public static Customer createCustomer(String name, String surname, String email){
+        if(!Helper.isValidEmail(email))
+            return null;
+
+            if(Helper.isNullorEmpty(name)|| Helper.isNullorEmpty(surname))
+                return null;
+
+        return new Customer.Builder().setName(name).setSurname(surname).setEmail(email).build();
+    }
+}
