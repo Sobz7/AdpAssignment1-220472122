7402604b2717bb143cd01357188fc8f50006013d
Soban <sobzsa7@gmail.com>
2023-03-26 03:15:44 +0200

    Payment and Shipping Factory Classes Added.

--- src/main/java/za/ac/cput/factory/ShippingFactory.java
+++ src/main/java/za/ac/cput/factory/ShippingFactory.java
@@ -5,5 +5,18 @@
 
 package za.ac.cput.factory;
 
+import za.ac.cput.domain.Shipping;
+
+import java.time.LocalDate;
+
 public class ShippingFactory {
+    public static Shipping createShipping(String shippingID, String orderID, String carrierName, Integer trackingNumber, LocalDate shippingDate, LocalDate deliveryDate){
+        return new Shipping.Builder().setshippingID(shippingID)
+                .setorderID(orderID)
+                .setcarrierName(carrierName)
+                .settrackingNumber(trackingNumber)
+                .setshippingDate(shippingDate)
+                .setdeliveryDate(deliveryDate)
+                .build();
+    }
 }
